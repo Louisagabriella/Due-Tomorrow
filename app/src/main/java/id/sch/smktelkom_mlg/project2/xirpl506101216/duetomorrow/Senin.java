04b17c7caa9f412cd53c52c6b045933c952dad61
louisa <louisa_gabriella_24rpl@student.smktelkom-mlg.sch.id>
2017-03-28 19:12:49 +0700

    adddata

--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl506101216/duetomorrow/Senin.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl506101216/duetomorrow/Senin.java
@@ -1,13 +1,66 @@
 package id.sch.smktelkom_mlg.project2.xirpl506101216.duetomorrow;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Senin extends AppCompatActivity {
+public class Senin extends Activity implements OnClickListener {
+    //inisilisasi elemen-elemen pada layout
+    private Button buttonSubmit;
+    private EditText edJam;
+    private EditText edNama;
+    //inisialisasi kontroller/Data Source
+    private DBDataSource dataSource;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_senin);
+
+        buttonSubmit = (Button) findViewById(R.id.buttom_submit);
+        buttonSubmit.setOnClickListener(this);
+        edNama = (EditText) findViewById(R.id.jam);
+        edJam = (EditText) findViewById(R.id.nama);
+
+        // instanstiasi kelas DBDataSource
+        dataSource = new DBDataSource(this);
+
+        //membuat sambungan baru ke database
+        dataSource.open();
+    }
+
+    //KETIKA Tombol Submit Diklik
+    @Override
+    public void onClick(View v) {
+        // Inisialisasi data barang
+        String jam = null;
+        String nama = null;
+        @SuppressWarnings("unused")
+
+        //inisialisasi barang baru (masih kosong)
+                Barang barang = null;
+        if (edNama.getText() != null && edJam.getText() != null) {
+            /* jika field nama, merk, dan harga tidak kosong
+             * maka masukkan ke dalam data barang*/
+            jam = edJam.getText().toString();
+            nama = edNama.getText().toString();
+        }
+
+        switch (v.getId()) {
+            case R.id.buttom_submit:
+                // insert data barang baru
+                barang = dataSource.createBarang(jam, nama);
+
+                //konfirmasi kesuksesan
+                Toast.makeText(this, "Data Berhasil Ditambahkan\n" +
+                        "Jam Ke-" + barang.getJamke() +
+                        "Nama Pelajaran" + barang.getNama_pelajaran(), Toast.LENGTH_LONG).show();
+                break;
+        }
+
     }
 }
